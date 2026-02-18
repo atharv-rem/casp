4f4de792e78669f047968a581e26551b02dfdb61
Atharv Remeshan <atharv.rem@gmail.com>
2026-02-18 18:41:14 +0530

    feat: add loading component for dashboard with centered message

---
+++ casp/app/dashboard/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex h-full w-full items-center justify-center">
+      <p className="text-[30px] font-medium text-gray-500 font-rethink">Launching your dashboard...</p>
+    </div>
+  );
+}
